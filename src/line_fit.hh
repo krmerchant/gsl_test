01c6a4f80098ed76fcd007cec2382f7970d1f490
TheCarmelGuy <komelm13@gmail.com>
2025-02-02 12:59:11 -0500

    fixed unit test

--- src/line_fit.hh
+++ src/line_fit.hh
@@ -2,7 +2,7 @@
 #define LINE_FIT_HH_
 
 #include <vector>
-#include <vector>
+#include <gsl/gsl_multimin.h>
 
 struct Line
 {
@@ -25,7 +25,7 @@ class LineFit
 {
   public:
     LineFit(){};
-    virtual Line ComputeLine(std::vector<double> _x, std::vector<double> _y) = 0;
+    virtual Line ComputeLine(std::vector<double> _x, std::vector<double> _y) const = 0;
 
 };
 
@@ -36,14 +36,104 @@ class NelderMeadLineFit : public LineFit
 
   public: 
    NelderMeadLineFit(){};
-   Line ComputeLine(std::vector<double> _x, std::vector<double> _y)
+   Line ComputeLine(std::vector<double> _x, std::vector<double> _y) const
    {
-    return Line(0,0);
+
+    auto [m,b] = RunOptimizer(_x,_y);
+    return Line(m,b);
+   }
+
+
+  struct Params
+  {
+    std::vector<double> x; 
+    std::vector<double> y; 
+    Params(std::vector<double>_x, std::vector<double> _y):x(_x),y(_y){}
+
+  };
+
+
+
+   std::tuple<double,double> RunOptimizer(std::vector<double> _x, std::vector<double> _y) const
+   {
+     const gsl_multimin_fminimizer_type *T =
+         gsl_multimin_fminimizer_nmsimplex2;
+     gsl_multimin_fminimizer *s = NULL;
+     gsl_vector *ss, *x;
+     gsl_multimin_function minex_func;
+
+    
+     size_t iter = 0;
+     int status;
+     double size;
+
+     /* Starting point */
+     x = gsl_vector_alloc(2); //m and b
+     gsl_vector_set(x, 0, 0.0);
+     gsl_vector_set(x, 1, 0.0);
+
+     /* Set initial step sizes to 1 */
+     ss = gsl_vector_alloc(2);
+     gsl_vector_set_all(ss, 1.0);
+
+     /* Initialize method and iterate */
+
+     minex_func.n = 2;
+     minex_func.f = [](const gsl_vector *modelParams, void *params) 
+     {
+       double m =  gsl_vector_get(modelParams, 0);
+       double b =  gsl_vector_get(modelParams, 1);
+       double cost = 0;
+       Params * data = static_cast<Params*>(params);
+       for (int i = 0; i < data->x.size() ; i++)
+       {
+        double fit = m*data->x[i] + b;
+        cost+= std::abs(fit - data->y[i]);
+
+       }
+      return cost;
+
+     };
+
+     Params params(_x,_y);
+     minex_func.params = &params;
+
+     s = gsl_multimin_fminimizer_alloc(T, 2);
+     gsl_multimin_fminimizer_set(s, &minex_func, x, ss);
+
+     do
+     {
+       iter++;
+       status = gsl_multimin_fminimizer_iterate(s);
+
+       if (status)
+         break;
+
+       size = gsl_multimin_fminimizer_size(s);
+       status = gsl_multimin_test_size(size, 1e-5);
+
+       if (status == GSL_SUCCESS)
+       {
+         printf("converged to minimum at\n");
+       }
+
+       printf("%5d %10.3e %10.3e f() = %7.3f size = %.3f\n",
+              iter,
+              gsl_vector_get(s->x, 0),
+              gsl_vector_get(s->x, 1),
+              s->fval, size);
+     } while (status == GSL_CONTINUE && iter < 1000);
+    
+
+
+     double m = gsl_vector_get(s->x, 0);
+     double b = gsl_vector_get(s->x, 1);
+     gsl_vector_free(x);
+     gsl_vector_free(ss);
+     gsl_multimin_fminimizer_free(s);
+     return std::make_tuple(m, b);
    }
 
 };
-
-
-
 
 #endif 
