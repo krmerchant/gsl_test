8f64d3a05370613880f9a14918fb3d5a4de69b8d
TheCarmelGuy <komelm13@gmail.com>
2025-02-02 03:26:06 -0500

    added gsl dep

---
+++ build/CMakeFiles/test_gsl.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_gsl.dir/src/main.cpp.o"
+  "test_gsl"
+  "test_gsl.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_gsl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
